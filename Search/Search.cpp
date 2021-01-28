1ff0cc332a38d06d1737ad473265a11e10dd9c73
SkyRock <skyrockpk@gmail.com>
2021-01-29 00:04:20 +0900

    add all my files

---
+++ Search/Search.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+bool IsKPeriodic(string str, int K)
+{
+	if ((str.size() % K || (str.size() == K)) > 0)
+	{
+		return false;
+	}
+
+	for (int i = 0; i < str.size() - K; i+=K)
+	{
+		int j = 0;
+		int num = i;
+		while (j != K)
+		{
+			if (str[num] != str[num + K])
+			{
+				return false;
+			}
+			j++;
+			num++;
+		}
+	}
+	return true;
+}
+
+
+int main()
+{
+	string string = "abc";
+	if (IsKPeriodic(string,3))
+	{
+		cout << "yes";
+	}
+	else
+	{
+		cout << "no";
+	}
+}
